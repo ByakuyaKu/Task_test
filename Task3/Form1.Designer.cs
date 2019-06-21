55d4a4d753eb707d77732758597e8c99b7a1ac6d
Rodion <fil@altlinux.org>
2019-06-21 16:43:51 +0300

    Created

---
+++ Task3/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(149, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Удалить одинаковые слова";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(251, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(212, 114);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 28);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(212, 114);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите слова";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Результат";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 188);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Task3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Task3/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string EnteredWords;
+        List<string> SplitWords = new List<string>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            EnteredWords = richTextBox2.Text;
+            SplitString(EnteredWords, SplitWords);
+            Equal(SplitWords);
+            EnteredWords = "";
+        }
+
+        string tmp;
+        void SplitString(string s, List<string> arr) // можно использовать String.Split
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ' || s[i] == '.' || 
+                    s[i] == ',' || s[i] == '?' || 
+                    s[i] == '!' || s[i] == ':' || 
+                    s[i] == '.' || s[i] == ';')
+                {
+                    arr.Add(tmp);
+                    tmp = "";
+                    continue;
+                }
+                tmp += s[i];
+            }
+            if (s[s.Length - 1] != ' ' || s[s.Length - 1] != '.' || s[s.Length - 1] != ',' || //для добавление последнего слова в строке если в конце нет знака препинания
+                s[s.Length - 1] != '?' || s[s.Length - 1] != '!' || s[s.Length - 1] != ':' ||
+                s[s.Length - 1] != ';')
+            {
+                arr.Add(tmp);
+            }
+        }
+
+        void Equal(List<string> array) // можно использовать String.Equals
+        {
+            for (int i = 0; i < array.Count; i++)
+            {
+                bool z = true;
+                for (int j = 0; j < array.Count; j++)
+                {
+                    if (array[i] == array[j] && i != j)
+                    {
+                        z = false;
+                    }
+
+                }
+                if (z)
+                {
+                    richTextBox1.Text += array[i] + ' ';
+                }
+            }
+        }
+    }
+}
